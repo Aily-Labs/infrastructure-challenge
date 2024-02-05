20b19a5e7eeebf6e06194c03ba21a8de6775af5a
jeffersonferrari <jefferson_ferrari@outlook.com>
2024-02-05 17:18:00 +0100

    adding simple script to build and push docker images

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -1,61 +1,69 @@
 #!/bin/bash
 
-# Nome do repositório ECR para a Flask API
-flask_api_repository="flask-api"
+docker_build_push() {
+  local REPOSITORY=$1
+  local IMAGE_TAG=$2
+  echo -e "\The Docker build will start in the repository ${REPOSITORY} with the tag ${IMAGE_TAG}."
+  docker build -t ${REPOSITORY}:${IMAGE_TAG} .
+  docker tag ${REPOSITORY}:${IMAGE_TAG} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:${IMAGE_TAG}
+  docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:${IMAGE_TAG}
+}
 
-# Nome do repositório ECR para o frontend
-frontend_repository="frontend"
+REPOSITORY=""
+AWS_REGION=""
+IMAGE_TAG=""
+CURRENT_DATE=$(date)
 
-# Verifique se a imagem Flask API com a tag 0.1 já existe
-if ! aws ecr describe-images --repository-name $flask_api_repository --image-ids imageTag=0.1 >/dev/null 2>&1; then
-  # Se a imagem não existe, faça o build, tag e push com a tag 0.1
-  docker build -t $flask_api_repository:0.1 .
-  docker tag $flask_api_repository:0.1 $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$flask_api_repository:0.1
-  docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$flask_api_repository:0.1
-else
-  # Se a imagem com a tag 0.1 já existe, encontre a próxima tag disponível
-  for i in {2..9}; do
-    if ! aws ecr describe-images --repository-name $flask_api_repository --image-ids imageTag=0.$i >/dev/null 2>&1; then
-      next_tag="0.$i"
-      break
-    fi
-  done
+for arg in "$@"; do
+    case $arg in  
+        repository=*)
+        REPOSITORY="${arg#*=}"
+        shift
+        ;;
+        aws_region=*)
+        AWS_REGION="${arg#*=}"
+        shift
+        ;;        
+        image_tag=*)
+        IMAGE_TAG="${arg#*=}"
+        shift
+        ;;
+        *)
+        ;;
+    esac
+done
 
-  # Se não houver mais tags disponíveis entre 0.2 e 0.9, vá para 1.0 e assim por diante
-  if [ -z "$next_tag" ]; then
-    latest_tag=$(aws ecr describe-images --repository-name $flask_api_repository --query 'images[].imageTags' --output json | jq -r '.[]' | grep -oE '^[0-9]+\.[0-9]+' | sort -V | tail -n 1)
-    major_version=$(echo $latest_tag | cut -d'.' -f1)
-    next_major_version=$((major_version + 1))
-    next_tag="${next_major_version}.0"
-  fi
+REPOSITORY=$(echo "$REPOSITORY" | tr '[:upper:]' '[:lower:]')
 
-  # Faça o build, tag e push com a próxima tag disponível
-  docker build -t $flask_api_repository:$next_tag .
-  docker tag $flask_api_repository:$next_tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$flask_api_repository:$next_tag
-  docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$flask_api_repository:$next_tag
+if [[ " ${REPOSITORY} " == "" ]]; then
+    echo -e "\n----> Please specify a value for repository"
+    echo -e "* ${CURRENT_DATE} - ERROR: repository is empty\n"
+    exit 1
 fi
 
-# Repita o mesmo processo para o repositório do frontend
-if ! aws ecr describe-images --repository-name $frontend_repository --image-ids imageTag=0.1 >/dev/null 2>&1; then
-  docker build -t $frontend_repository:0.1 .
-  docker tag $frontend_repository:0.1 $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$frontend_repository:0.1
-  docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$frontend_repository:0.1
-else
-  for i in {2..9}; do
-    if ! aws ecr describe-images --repository-name $frontend_repository --image-ids imageTag=0.$i >/dev/null 2>&1; then
-      next_tag="0.$i"
-      break
+AWS_REGION=$(echo "$AWS_REGION" | tr '[:upper:]' '[:lower:]')
+
+if [[ " ${AWS_REGION} " == "" ]]; then
+    echo -e "\n----> Please specify a region. eg aws_region=us-east-1"
+    echo -e "* ${CURRENT_DATE} - ERROR: aws_region is empty\n"
+    exit 1
+fi
+
+if [[ ! " ${IMAGE_TAG} " == "" ]]; then
+    IMAGE_TAG="0.1"
+fi
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+
+for first_number in {0..10}; do
+  for second_number in {0..9}; do
+    IMAGE_TAG="${major}.${minor}"
+    if ! aws ecr describe-images --repository-name ${REPOSITORY} --image-ids imageTag=${IMAGE_TAG} >/dev/null 2>&1; then
+      docker_build_push ${REPOSITORY} ${IMAGE_TAG}
+      exit 0
     fi
   done
+done
 
-  if [ -z "$next_tag" ]; then
-    latest_tag=$(aws ecr describe-images --repository-name $frontend_repository --query 'images[].imageTags' --output json | jq -r '.[]' | grep -oE '^[0-9]+\.[0-9]+' | sort -V | tail -n 1)
-    major_version=$(echo $latest_tag | cut -d'.' -f1)
-    next_major_version=$((major_version + 1))
-    next_tag="${next_major_version}.0"
-  fi
-
-  docker build -t $frontend_repository:$next_tag .
-  docker tag $frontend_repository:$next_tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$frontend_repository:$next_tag
-  docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$frontend_repository:$next_tag
-fi
+echo -e "\n* ${CURRENT_DATE} - ERROR: No available tags found for range 0.1 to 10.9"
+exit 1
